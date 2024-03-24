a7d958b2d56fbf0db7ca8f50bd80a7ecccb1a185
Dmytro Korolik <dimytry.korolik@gmail.com>
2024-03-25 01:06:06 +0200

    favorites-page

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,7 +1,37 @@
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { CamperCardsWrapper } from './Favorites.styled';
+import { ButtonLoadMore, CamperCards } from '../../components';
+import { selectFavorites } from '../../redux/selectors';
+
 const Favorites = () => {
+  const [limit] = useState(4);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [items, setItems] = useState([]);
+
+  const favorites = useSelector(selectFavorites);
+
+  useEffect(() => {
+    setItems((prevItems) => [
+      ...prevItems,
+      ...favorites.slice(currentPage * limit - limit, currentPage * limit),
+    ]);
+  }, [currentPage, limit, favorites]);
+
+  const handleLoadMore = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
   return (
     <>
-      <h1>Favorites</h1>
+      {favorites.length > 0 && (
+        <CamperCardsWrapper>
+          <CamperCards campers={items} />
+          {currentPage * limit < favorites.length && (
+            <ButtonLoadMore onClick={handleLoadMore} />
+          )}
+        </CamperCardsWrapper>
+      )}
     </>
   );
 };
---
+++ src/pages/Favorites/Favorites.styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const CamperCardsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  gap: 50px;
+`;
