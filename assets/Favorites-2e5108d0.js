c7d867d7aa995c31eacd80727624be94635cffd3
KorolikD <96023073+KorolikD@users.noreply.github.com>
2024-03-24 17:18:08 +0000

    Deploying to gh-pages from @ KorolikD/rental-camper@c9d2cedfee0431dda1f85bd4a3b8089b1cf80f65 🚀

--- assets/Favorites-2e5108d0.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-7e2c8d4a.js";const e=()=>r.jsx(r.Fragment,{children:r.jsx("h1",{children:"Favorites"})});export{e as default};
--- assets/Home-8d8a87bf.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-7e2c8d4a.js";const t=()=>e.jsx(e.Fragment,{children:e.jsx("h1",{children:"Home"})});export{t as default};
